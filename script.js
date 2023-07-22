2e6416aa3d87ee1ae47408d2584762bc72a14bf0
Romesh Jain <romeshjainofficial@gmail.com>
2023-07-22 20:22:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.getElementById("sign").onclick = function(){
+//     let hamMenu = document.getElementById("hamMenu");
+//     hamMenu.style.display = "block";
+//     hamMenu.style.backgroundColor = "red";
+//     sign.style.opacity = ".8";
+// }
