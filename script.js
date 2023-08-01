ab21b7583ef5cb29fc2d3d7dfe208dce2b6ddfef
romeshjainn <romeshjainofficial@gmail.com>
2023-08-01 12:18:25 +0530

    deleted unnecessary files

--- script.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("sign").onclick = function(){
-//     let hamMenu = document.getElementById("hamMenu");
-//     hamMenu.style.display = "block";
-//     hamMenu.style.backgroundColor = "red";
-//     sign.style.opacity = ".8";
-// }
